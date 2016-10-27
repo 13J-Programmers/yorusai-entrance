f5d302213679e042d5093930ec973c0759b7e5ea
nobuyo <world_fantasy_2017@live.jp>
2016-10-27 22:03:14 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root 'application#index'
 
+  get 'usage' => 'application#usage', :as => 'usage'
+
   resources :admins, only: [:index, :edit, :show, :update]
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
   get 'students/index' => 'students#index', :as => 'students_index'
-
-  get 'students/scan' => 'students#scan'
-
+  get 'students/scan' => 'students#scan', :as => 'scan'
   get 'students' => 'students#enter'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
