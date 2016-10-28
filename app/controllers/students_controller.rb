3cede923ae0f7325be39a811928e1106d010a18b
nobuyo <world_fantasy_2017@live.jp>
2016-10-28 09:35:16 +0900

    fix bug

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,20 +1,24 @@
 class StudentsController < ApplicationController
+
+  protect_from_forgery :except => [:update]
+
   def index
     @students = Student.all
   end
 
   def scan
-    if @student = Student.find_by(student_id: params[:student_id]) then
+  end
+
+  def update
+    p params
+    if @student = Student.find_by(student_id: params[:student][:student_id]) then
       @student.attended = "true"
       @student.save
     end
   end
 
-  def update
-  end
-
   private
     def student_params
-      params.require(:student).permit(:student_id)
+      params.require(:student).permit(:student_id, :attended, :elected)
     end
 end
