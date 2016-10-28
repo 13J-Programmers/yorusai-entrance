a3ab97aab849dbabb278d23bee4e49a9e4247680
Mako <mnfeconicu41@gmail.com>
2016-10-28 23:54:51 +0900

    add admins_controller_test

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = admins(:admin)
+  end
+
+  test "should require authorization before get index" do
+    get admins_url
+    assert_redirected_to login_url
+  end
+
+  test "should require authorization before get show" do
+    get admin_url(@admin)
+    assert_redirected_to login_url
+  end
+
+  test "should require authorization before get edit" do
+    get edit_admin_url(@admin)
+    assert_redirected_to login_url
+  end
+
 end
