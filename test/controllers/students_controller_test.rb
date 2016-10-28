67217a0c64e658159e771b11813a366413b14cea
Mako <mnfeconicu41@gmail.com>
2016-10-28 22:02:10 +0900

    update test to be green

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,19 +1,14 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get students_index_url
-    assert_response :success
+  test "should require authorization before get index" do
+    get students_url
+    assert_redirected_to login_url
   end
 
-  test "should get scan" do
+  test "should require authorization before get scan" do
     get students_scan_url
-    assert_response :success
-  end
-
-  test "should get enter" do
-    get students_enter_url
-    assert_response :success
+    assert_redirected_to login_url
   end
 
 end
