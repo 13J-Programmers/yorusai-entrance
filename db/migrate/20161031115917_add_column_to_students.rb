b169be71d34c7737c70e67970827726a37a23eef
nobuyo <world_fantasy_2017@live.jp>
2016-10-31 21:24:34 +0900

    update migration file

--- db/migrate/20161031115917_add_column_to_students.rb
+++ db/migrate/20161031115917_add_column_to_students.rb
@@ -2,5 +2,6 @@ class AddColumnToStudents < ActiveRecord::Migration[5.0]
   def change
     add_column :students, :attended_at, :datetime
     add_column :students, :elected_at,  :datetime
+    add_column :students, :grade,       :integer # 1,2,3,4,5 + 6,7
   end
 end
