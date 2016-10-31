7a81bc2a7bc0a05faa98aef57e96c3e8651f9af0
nobuyo <world_fantasy_2017@live.jp>
2016-10-31 21:02:32 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029152620) do
+ActiveRecord::Schema.define(version: 20161031115917) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -29,12 +29,14 @@ ActiveRecord::Schema.define(version: 20161029152620) do
 
   create_table "students", force: :cascade do |t|
     t.integer  "student_id"
-    t.boolean  "attended",   default: false, null: false
-    t.boolean  "elected",    default: false, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.boolean  "attended",    default: false, null: false
+    t.boolean  "elected",     default: false, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "dept"
     t.integer  "class_id"
+    t.date     "attended_at"                  null: false
+    t.date     "elected_at"                   null: false
   end
 
 end
