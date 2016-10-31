93284f57a0fa9ec9c3091c5f1e5feb4366bd1ea5
Mako <mnfeconicu41@gmail.com>
2016-10-31 17:47:23 +0900

    classroom model rejects nil of elected flag

--- db/migrate/20161029152620_create_classrooms.rb
+++ db/migrate/20161029152620_create_classrooms.rb
@@ -2,7 +2,7 @@ class CreateClassrooms < ActiveRecord::Migration[5.0]
   def change
     create_table :classrooms do |t|
       t.string :class_id
-      t.boolean :elected
+      t.boolean :elected, null: false, default: false
 
       t.timestamps
     end
