920b615f1b8d9a680c2cea1832ffa11648cc43a0
Mako <mnfeconicu41@gmail.com>
2016-11-22 23:14:39 +0900

    modify Student and Classroom model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,15 @@
 class Classroom < ApplicationRecord
-  validates :class_id, presence: true, uniqueness: true
+  has_many :students
+
+  validates :grade, presence: true
+  validates :classname, presence: true
+
+  def abbr
+    case self.grade
+    when "1".."2"
+      "#{self.grade}-#{self.classname}" # => 1-1 1-2 ...
+    when "3".."5"
+      "#{self.grade}#{self.classname}" # => 3M 3E ...
+    end
+  end
 end
