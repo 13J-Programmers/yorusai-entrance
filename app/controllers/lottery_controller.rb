a7bfdaec8acb8a0123a1a235c4ee95f6c5d3efdc
Mako <mnfeconicu41@gmail.com>
2016-11-23 08:51:47 +0900

    modify code to close to the previous

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -29,6 +29,6 @@ class LotteryController < ApplicationController
 
   def result
     @winners = Student.where(elected: true).order(:elected_at).pluck(:student_id)
-    @classes = Classroom.where(elected: true).order(:updated_at)
+    @classes = Classroom.where(elected: true).order(:updated_at).map(&:abbr)
   end
 end
